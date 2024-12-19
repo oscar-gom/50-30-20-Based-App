2841642e2ad08180609588081536a3a1257c9bad
Óscar Gómez <oscargomezsedas@gmail.com>
2024-12-19 18:45:12 +0100

    created movement model

---
+++ app/src/main/java/com/oscargs/savingsapp/models/Movement.kt
@@ -0,0 +1,21 @@
+package com.oscargs.savingsapp.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.oscargs.savingsapp.utilities.Category
+import com.oscargs.savingsapp.utilities.MovementType
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+@Entity
+data class Movement(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val amount: Double,
+    val description: String,
+    val date: LocalDate,
+    val type: MovementType,
+    val category: Category,
+    val creationTime: LocalDateTime,
+    val modificationTime: LocalDateTime
+)
