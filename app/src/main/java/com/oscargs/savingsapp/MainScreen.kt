699af38bfa237b19ef7985e7523bb143572915cf
Óscar Gómez <oscargomezsedas@gmail.com>
2025-01-13 11:40:10 +0100

    created delete button

--- app/src/main/java/com/oscargs/savingsapp/MainScreen.kt
+++ app/src/main/java/com/oscargs/savingsapp/MainScreen.kt
@@ -17,10 +17,13 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
@@ -351,6 +354,14 @@ fun ItemDisplay(movement: Movement, onItemClick: (Int) -> Unit) {
                         }
                     )
             )
+            FilledTonalButton (
+                modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally),
+                onClick = {
+                    /*TODO*/
+                }
+            ) {
+                Icon(Icons.Filled.Delete, contentDescription = stringResource(id = R.string.deleteMovement))
+            }
         }
     }
 }
